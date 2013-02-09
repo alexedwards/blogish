1f6ace1221c3c9b27cde0cd2cc5b59d41b1d52b0
Alex Edwards <ajmedwards@gmail.com>
2013-02-09 13:06:40 +0000

    add: documentation for public methods

--- lib/blogish.rb
+++ lib/blogish.rb
@@ -5,10 +5,47 @@ require "blogish/rouge_html"
 module Blogish
   extend self
 
+  # Fetch and parse a single blog entry based on its slug.
+  #
+  # * @params slug [String]
+  # * @return [nil] if no matching slug is found
+  # * @return [Hash] representing the blog entry if a matching slug is found.
+  #
+  # == Example:
+  #
+  # If we have a file named
+  #     views/blog/2013-02-07_hello-world.mkd
+  # then running
+  #     Blogish.fetch('hello-world')
+  # will find the file and return a hash containing the following keys:
+  #
+  # * :title [String]
+  # * :date [Date]
+  # * :slug [String]
+  # * :intro [String]
+  # * :body [String]
+
   def fetch(slug)
     path = entries.detect { |file| file =~ /_#{Regexp.quote(slug)}.mkd$/ }
     path ? open(path) : nil
   end
+
+  # Fetch and parse all blog entries in the `views/blog` directory, sorted
+  # by date with the most recent entry first.
+  #
+  # * @params none
+  # * @return [Array] of hashes representing each blog entry.
+  #
+  # == Example:
+  #
+  # If we have files named
+  #     views/blog/2013-02-07_hello-world.mkd
+  # and
+  #     views/blog/2013-02-05_foo-post.mkd
+  # then running
+  #     Blogish.fetch
+  # will return an Array containing both entries:
+  #     [{:title => 'Foo Post', ...}, {:title => 'Hello World', ...}]
 
   def fetch_all
     entries.sort.reverse.map { |entry| open(entry) }
